37334247aabfa4f1d4e94bb032ac6de241e3af80
EsGui <guilhermedagama2021100@gmail.com>
2024-04-27 21:53:13 -0300

    react navigation instalado e configurado

---
+++ components/home_components/ListNotes.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+export default function ListNotes() {
+    return (
+        <View>
+            <Text>Listagem das notas</Text>
+        </View>
+    )
+}
